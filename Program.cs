fe4161cc0402a60890d1089ff4a28ffbd736badd
GokulVelavan <gokulvelavans@gmail.com>
2021-10-18 12:46:17 +0530

    Welcome Message

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comparision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LineComparison Computation Program");
+            Console.ReadLine();
+        }
+    }
+}
