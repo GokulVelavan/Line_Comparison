3cbb570338f6b7c815aee7c5c568b0ef66d9d336
GokulVelavan <gokulvelavans@gmail.com>
2021-10-19 20:26:32 +0530

    checking equality of two number

--- Program.cs
+++ Program.cs
@@ -11,13 +11,28 @@ namespace Line_Comparision
         static void Main(string[] args)
         {
             double x1, y1, x2, y2,z;
-            Console.WriteLine("Enter the Points x1,y1, and x2,y2");//getting input from the user
+            double x3, y3, x4, y4, z2;
+
+            Console.WriteLine("Enter the Points x1,y1, and x2,y2 to find the Length of the First Line");//getting input from the user
             x1 = double.Parse(Console.ReadLine());
             y1 = double.Parse(Console.ReadLine());
             x2 = double.Parse(Console.ReadLine());
             y2 = double.Parse(Console.ReadLine());
-            z = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2));//length of the line
-            Console.WriteLine("Length of the line:{0}",z);
+            Console.WriteLine("Enter the Points x3,y3, and x4,y4 to find the Length of the Second Line");//getting input from the user
+            x3 = double.Parse(Console.ReadLine());
+            y3 = double.Parse(Console.ReadLine());
+            x4 = double.Parse(Console.ReadLine());
+            y4 = double.Parse(Console.ReadLine());
+            z = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2));//length of the 1st line 
+            z2 = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));//length of the 2st line 
+            if (z == z2)
+            {
+            Console.WriteLine("Two Lines Are Equal");
+            }
+            else
+            {
+                Console.WriteLine("Two Lines Are Not Equal");
+            }
             Console.ReadLine();
         }
     }
